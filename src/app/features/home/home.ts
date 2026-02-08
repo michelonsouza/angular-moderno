106323319c9d57c8a92539b30207ec849f76f5a2
Michelon Souza <michelon.souza@hotmail.com>
2026-02-07 22:30:43 -0300

    feat(home): uses balance card in home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { BalanceCard } from './components/balance-card/balance-card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [BalanceCard],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
-
-}
+export class Home {}
