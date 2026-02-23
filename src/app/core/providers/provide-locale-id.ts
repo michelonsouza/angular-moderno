6e2ddef8dee21241309a851cfc5025a652cd5e39
Michelon Souza <michelon.souza@hotmail.com>
2026-02-23 14:34:18 -0300

    feat: finish pipe module

--- src/app/core/providers/provide-locale-id.ts
+++ src/app/core/providers/provide-locale-id.ts
@@ -3,6 +3,6 @@ import { LOCALE_ID, Provider } from '@angular/core';
 export function provideLocaleId(): Provider {
   return {
     provide: LOCALE_ID,
-    useValue: 'pt',
+    useValue: 'pt-BR',
   };
 }
--- src/app/features/home/pages/list/components/balance/components/balance-card/balance-card.ts
+++ src/app/features/home/pages/list/components/balance/components/balance-card/balance-card.ts
@@ -1,13 +1,13 @@
 import type { TransactionType } from '@shared/transaction/interfaces/transaction';
 import { Component, computed, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { CurrencyPipe } from '@angular/common';
+import { HumanizeCurrencyPipe } from './pipes/humanize-currency-pipe';
 
 type BalanceCardCssClass = 'income' | 'outcome' | 'zero';
 
 @Component({
   selector: 'app-balance-card',
-  imports: [MatCardModule, CurrencyPipe],
+  imports: [MatCardModule, HumanizeCurrencyPipe],
   templateUrl: './balance-card.html',
   styleUrl: './balance-card.scss',
 })
